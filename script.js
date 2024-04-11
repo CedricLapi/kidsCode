2690e9323dc62ea7d41bd6a1d2e58d62bde751b5
factor <lapicedric>
2024-04-11 09:46:43 +0100

    a

---
+++ script.js
@@ -0,0 +1,3 @@
+// Add JavaScript interactions or animations here
+// Example: Add scroll reveal animation to the "About" section
+ScrollReveal().reveal('#about', { delay: 300, distance: '50px', easing: 'ease-in' });
